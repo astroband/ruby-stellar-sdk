18d6d3c623273d96e39e1bab6d07e8b986ac1448
Scott Fleckenstein <nullstyle@gmail.com>
2015-03-22 14:55:20 -0700

    Add some display helpers to currency

--- lib/stellar/currency.rb
+++ lib/stellar/currency.rb
@@ -9,5 +9,20 @@ module Stellar
       ici = ISOCurrencyIssuer.new({currency_code:code, issuer:issuer.public_key})
       new(:iso4217, ici)
     end
+
+    def to_s
+      if switch == CurrencyType.native
+        "native"
+      else
+        encoder = Stellar::Util::Base58.stellar
+        issuer_address = encoder.check_encode(:account_id,iso_ci.issuer)
+        "#{iso_ci.currency_code}/#{issuer_address}"
+      end
+    end
+
+    def inspect
+      label = switch.to_s
+      "#<Stellar::Currency #{to_s}>"
+    end
   end
 end
