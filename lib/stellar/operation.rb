8b37bdd795cc201ef543818e0a18990aa748d452
Scott Fleckenstein <nullstyle@gmail.com>
2015-05-02 10:40:28 -0700

    Move operation helpers from Transaction to Operation

---
+++ lib/stellar/operation.rb
@@ -0,0 +1,48 @@
+module Stellar
+  Operation.class_eval do
+
+    def self.payment(attributes={})
+      destination = attributes[:destination]
+      amount      = attributes[:amount]
+      path        = attributes[:path] || []
+      path        = path.map{|p| Stellar::Currency.send(*p)}
+
+      raise ArgumentError unless destination.is_a?(KeyPair)
+
+      op = PaymentOp.send(*amount)
+      op.destination = destination.public_key
+      op.path = path
+      op.apply_defaults
+
+      op.to_operation
+    end
+
+    def self.change_trust(attributes={})
+      line  = Currency.send(*attributes[:line])
+      limit = attributes[:limit]
+
+      raise ArgumentError, "Bad :limit #{limit}" unless limit.is_a?(Integer)
+
+      op = ChangeTrustOp.new(line: line, limit: limit)
+      op.to_operation
+    end
+
+    def self.create_offer(attributes={})
+      taker_pays = Currency.send(*attributes[:taker_pays])
+      taker_gets = Currency.send(*attributes[:taker_gets])
+      amount     = attributes[:amount]
+      offer_id   = attributes[:offer_id] || 0
+      price      = Price.from_f(attributes[:price])
+
+      op = CreateOfferOp.new({
+        taker_pays: taker_pays, 
+        taker_gets: taker_gets,
+        amount:     amount,
+        price:      price,
+        offer_id:   offer_id
+      })
+      op.to_operation
+    end
+
+  end
+end
--- lib/stellar/transaction.rb
+++ lib/stellar/transaction.rb
@@ -2,51 +2,20 @@ module Stellar
   Transaction.class_eval do
 
     def self.payment(attributes={})
-      destination = attributes[:destination]
-      amount      = attributes[:amount]
-      path        = attributes[:path] || []
-      path        = path.map{|p| Stellar::Currency.send(*p)}
-
-      raise ArgumentError unless destination.is_a?(KeyPair)
-
       for_account(attributes).tap do |result|
-        payment = PaymentOp.send(*amount)
-        payment.destination = destination.public_key
-        payment.path = path
-        payment.apply_defaults
-
-        result.operations = [payment.to_operation]
+        result.operations = [Operation.payment(attributes)]
       end
     end
 
     def self.change_trust(attributes={})
-      line  = Currency.send(*attributes[:line])
-      limit = attributes[:limit]
-
-      raise ArgumentError, "Bad :limit #{limit}" unless limit.is_a?(Integer)
-
       for_account(attributes).tap do |result|
-        details = ChangeTrustOp.new(line: line, limit: limit)
-        result.operations = [details.to_operation]
+        result.operations = [Operation.change_trust(attributes)]
       end
     end
 
     def self.create_offer(attributes={})
-      taker_pays = Currency.send(*attributes[:taker_pays])
-      taker_gets = Currency.send(*attributes[:taker_gets])
-      amount     = attributes[:amount]
-      offer_id   = attributes[:offer_id] || 0
-      price      = Price.from_f(attributes[:price])
-
       for_account(attributes).tap do |result|
-        details = CreateOfferOp.new({
-          taker_pays: taker_pays, 
-          taker_gets: taker_gets,
-          amount:     amount,
-          price:      price,
-          offer_id:   offer_id
-        })
-        result.operations = [details.to_operation]
+        result.operations = [Operation.create_offer(attributes)]
       end
     end
 
