ded1c924fb1ea4b5314328c6a61afd513b69cb94
Scott Fleckenstein <nullstyle@gmail.com>
2015-07-24 11:31:26 -0700

    Fix create_account sample

--- examples/create_account.rb
+++ examples/create_account.rb
@@ -10,13 +10,13 @@ $server = Faraday.new(url: "http://localhost:39132") do |conn|
   conn.adapter Faraday.default_adapter
 end
 
-master      = Stellar::KeyPair.from_raw_seed("allmylifemyhearthasbeensearching")
+master      = Stellar::KeyPair.master
 destination = Stellar::KeyPair.random
 
 tx = Stellar::Transaction.create_account({
   account:     master,
   destination: destination,
-  sequence:    2,
+  sequence:    1,
   starting_balance:  50 * Stellar::ONE
 })
 
