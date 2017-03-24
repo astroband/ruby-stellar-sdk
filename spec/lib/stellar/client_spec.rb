2c545d72a3e04dc80b670eaace86e16b89d8bf80
Ramon Tayag <ramon.tayag@gmail.com>
2017-03-24 16:20:40 +0800

    Actually put a test in `#create_account`
    - Had to loosen the request matching even more, because the recipient of the
      funds is a random account

--- spec/lib/stellar/client_spec.rb
+++ spec/lib/stellar/client_spec.rb
@@ -8,12 +8,20 @@ describe Stellar::Client do
     let(:funder) { Stellar::Account.from_seed(CONFIG[:funded_seed]) }
     let(:account) { Stellar::Account.random }
 
-    it "creates the account", vcr: {record: :once} do
+    it "creates the account", vcr: {record: :once, match_requests_on: [:method]} do
       client.create_account(
         funder: funder,
         account: account,
         starting_balance: 100,
       )
+
+      account_info = client.account_info(account)
+      balances = account_info.balances
+      expect(balances).to_not be_empty
+      native_asset_balance_info = balances.find do |b|
+        b["asset_type"] == "native"
+      end
+      expect(native_asset_balance_info["balance"].to_f).to eq 100.0
     end
   end
 
