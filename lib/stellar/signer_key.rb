98e0f75c6f23908a3d8cdd9873dfa1df73504abe
Scott Fleckenstein <nullstyle@gmail.com>
2018-07-30 15:18:44 -0800

    Fix hash_x signer, add simple tests

--- lib/stellar/signer_key.rb
+++ lib/stellar/signer_key.rb
@@ -1,6 +1,8 @@
 module Stellar
   class SignerKey
 
+    PREIMAGE_LENGTH = 32
+
     def self.ed25519(keypair)
       raise ArgumentError, "Bad keypair" unless keypair.is_a?(KeyPair)
       new(:signer_key_type_ed25519, keypair.raw_public_key)
@@ -12,7 +14,10 @@ module Stellar
     end
 
 
-    def self.onetime_signer(preimage)
+    def self.hash_x(preimage)
+      raise ArgumentError, "Must be string" unless preimage.is_a?(String) 
+      raise ArgumentError, "Must be 32 bytes" unless preimage.bytesize == PREIMAGE_LENGTH
+      
       hash_x = Digest::SHA256.digest(preimage)
       new(:signer_key_type_hash_x, hash_x)
     end
@@ -34,6 +39,11 @@ module Stellar
     def inspect
       label = switch.to_s
       "#<Stellar::SignerKey #{to_s}>"
+    end
+
+    def signature_hint
+      # take last 4 bytes
+      value.to_xdr.slice(-4, 4)
     end
 
   end
