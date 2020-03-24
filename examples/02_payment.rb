46d5cc8c28d62b2d48e504961dd3ea5c878a1d5c
Jake Urban <jake@stellar.org>
2020-03-24 15:12:32 -0700

    updated payment example

--- examples/02_payment.rb
+++ examples/02_payment.rb
@@ -1,15 +1,35 @@
 require 'stellar-sdk'
 
-account = Stellar::Account.from_seed("SBXH4SEH32PENMMB66P4TY6LXUIFMRVFUMX2LJC3P2STHICBJLNQJOH5") 
 client  = Stellar::Client.default_testnet()
 
-# create a random recipients
-recipient = Stellar::Account.random
+puts "Creating random sender..."
+from = Stellar::KeyPair.random
+client.friendbot(from)
+puts "Creating random recipient..."
+recipient = Stellar::KeyPair.random
+client.friendbot(from)
 
-# make a payment
-client.send_payment({
-  from:   account,
-  to:     recipient,
-  amount: Stellar::Amount.new(100_000_000)
-}) # => #<OK>
+puts "Retrieving account's current sequence number..."
+seq_num = client.account_info(from.address).sequence.to_i
 
+puts "Constructing transaction..."
+# construct TransactionBuilder and payment Operation
+builder = Stellar::TransactionBuilder.new(
+  source_account: from,
+  sequence_number: seq_num + 1
+)
+# Note: if you want to send a non-native asset, :amount must take the form: 
+# [<:alphanum12 or :alphanum4>, <code>, <issuer keypair>, <amount>]
+payment_op = Operation.payment({
+  destination: recipient,
+  amount: [:native, 100]
+})
+
+# add payment to transaction and set a 600ms timeout
+tx = builder.add_operation(payment_op).set_timeout(600).build()
+# sign transaction and get xdr
+envelope = tx.to_envelope(from).to_xdr(:base64)
+
+puts "Submitting transaction to horizon..."
+# submit the transaction
+client.horizon.transactions._post(tx: envelope)
