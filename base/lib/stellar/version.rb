7b8a26e0fd22abad870985022c38eec26554712e
Timur Ramazanov <charlie.vmk@gmail.com>
2020-06-02 15:24:18 +0300

    Bump versions

--- base/lib/stellar/version.rb
+++ base/lib/stellar/version.rb
@@ -1,5 +1,5 @@
 module Stellar
   module Base
-    VERSION = "0.23.0.pre1"
+    VERSION = "0.23.0.rc1"
   end
 end
--- sdk/lib/stellar/version.rb
+++ sdk/lib/stellar/version.rb
@@ -1,3 +1,3 @@
 module Stellar
-  VERSION = "0.9.0.pre2"
+  VERSION = "0.9.0.rc1"
 end
