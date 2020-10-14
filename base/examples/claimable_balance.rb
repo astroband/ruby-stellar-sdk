0f3dc5baff5bd1e63f5a3e23c02ab0f0ca3256e9
Sergey Nebolsin <nebolsin@gmail.com>
2020-10-14 18:50:42 +0300

    Claim balance operation, full claimable balance example

--- base/examples/claimable_balance.rb
+++ base/examples/claimable_balance.rb
@@ -5,21 +5,23 @@ require "json"
 require "net/http"
 require "uri"
 
+include Stellar::DSL
+
 base_endpoint = URI("https://horizon-testnet.stellar.org/")
 
 fetch_sequence = ->(account) do
-  response = Net::HTTP.get(base_endpoint + "accounts/#{account}")
+  address = KeyPair(account).address
+  response = Net::HTTP.get(base_endpoint + "accounts/" + address)
 
   JSON.parse(response, symbolize_names: true)[:sequence]&.to_i.tap { |res| ap "Got #{res} for #{account}" }
 end
 
 submit_tx = ->(tx_envelope) do
-  response = Net::HTTP.post_form(base_endpoint + 'transactions/', tx: tx_envelope.to_xdr(:base64))
+  response = Net::HTTP.post_form(base_endpoint + "transactions/", tx: tx_envelope.to_xdr(:base64))
 
   JSON.parse(response.body, symbolize_names: true)
 end
 
-include Stellar::DSL
 
 network_passphrase = Stellar::Networks::TESTNET
 sponsor_keypair = KeyPair("SAR2GU5YPVG2Y5I4OSQN2O3WTGSTTENSNUOVNO2LHPMC4B32W4Z2YNPB")
@@ -33,7 +35,7 @@ tx_create = Stellar::TransactionBuilder.create_claimable_balance(
     amount: 100,
     claimants: [
       Claimant(sponsor_keypair), # unconditional
-      Claimant(claimant_keypair) { before_relative_time(1.week) & ~before_relative_time(15.minutes) }
+      Claimant(claimant_keypair) { before_relative_time(1.week) & ~before_relative_time(10.seconds) }
     ]
   )
 
@@ -41,3 +43,25 @@ tx_create_envelop = tx_create.to_envelope(sponsor_keypair)
 tx_create_response = submit_tx.(tx_create_envelop)
 
 ap tx_create_response
+
+fail "Error creating claimable balance" unless tx_create_response[:successful]
+
+tx_result = Stellar::TransactionResult.from_xdr(tx_create_response[:result_xdr], "base64")
+balance_id = tx_result.result.results![0].tr!.create_claimable_balance_result!.balance_id!
+
+ap "Created Claimable Balance ID: #{balance_id.value.unpack("H*").first}, waiting 10 seconds"
+sleep 10
+
+tx_claim = Stellar::TransactionBuilder.claim_claimable_balance(
+  network_passphrase: network_passphrase,
+  source_account: claimant_keypair,
+  sequence_number: fetch_sequence.(claimant_keypair) + 1,
+  balance_id: balance_id,
+)
+
+tx_claim_envelop = tx_claim.to_envelope(claimant_keypair)
+tx_claim_response = submit_tx.(tx_claim_envelop)
+
+ap tx_claim_response
+
+fail "Error claiming the balance" unless tx_claim_response[:successful]
--- base/lib/stellar/ext/xdr.rb
+++ base/lib/stellar/ext/xdr.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/string/inflections"
+require "xdr"
 
 # For every member of XDR::Enum define constant on the corresponding enum type class.
 # Constant name is a member name stripped of the common prefix with enum type name.
@@ -10,4 +11,13 @@ XDR::DSL::Enum.redefine_method(:seal) do
     const_set(name.sub(common_prefix_regexp, "").upcase, value)
   end
   self.sealed = true
+end
+
+# XDR::Union generates an attribute method for each `arm`, but lacks the
+# actual `attribute(attr)` method those generated methods delegate to.
+# We follow the semantics of the bang variant `XDR::Union#attribute!` method,
+# except that calls to `raise` are replaced with early returns of nil.
+XDR::Union.define_method(:attribute) do |attr|
+  return unless @arm.to_s == attr
+  get
 end
